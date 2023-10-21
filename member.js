78211faa771858615e6ab37e5f917e1a0ddc3b40
Onile-ere Victor <116771477+Majorplaydev@users.noreply.github.com>
2023-10-21 14:36:06 +0000

    Copilot second commit

---
+++ member.js
@@ -0,0 +1,8 @@
+function skillsMember() {
+    return {
+        restrict: 'E',
+        templateUrl: 'views/skillsMember.html',
+        controller: 'SkillsMemberCtrl',
+        controllerAs: 'skillsMemberCtrl'
+    };
+}
